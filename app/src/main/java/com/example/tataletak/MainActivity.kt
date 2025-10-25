5fac5e5fec4ee8d5d8099e5015e03ffb1a0e5752
ZakyMalika <zakymalika0987@gmail.com>
2025-10-25 17:18:26 +0700

    mengganti main activity untuk menmpilkan tampilan file Tataletak2.kt pada emulator

--- app/src/main/java/com/example/tataletak/MainActivity.kt
+++ app/src/main/java/com/example/tataletak/MainActivity.kt
@@ -20,9 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TataletakTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TataletakBoxColumnRow(
-
-                        modifier = Modifier.padding(innerPadding)
+                    Tambah(modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
