2d8d9ac65d319ea5ea10cd9e27a43ba9bbb946a2
ZakyMalika <zakymalika0987@gmail.com>
2025-10-25 15:57:34 +0700

    membuat fun buat Column

--- app/src/main/java/com/example/tataletak/Tataletak1.kt
+++ app/src/main/java/com/example/tataletak/Tataletak1.kt
@@ -1,13 +1,16 @@
 package com.example.tataletak
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -21,13 +24,3 @@ fun TataletakColumn(modifier: Modifier){
     }
 }
 
-@Composable
-fun TataletakRow(modifier: Modifier){
-    Row(modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceEvenly) {
-        Text(text = "Komponen1")
-        Text(text = "Komponen2")
-        Text(text = "Komponen3")
-        Text(text = "Komponen4")
-    }
-}
