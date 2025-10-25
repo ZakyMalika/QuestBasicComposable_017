5e2b9f8786965c87d029db9591d47ad79d62cd2a
ZakyMalika <zakymalika0987@gmail.com>
2025-10-25 16:05:58 +0700

    membuat isi text column di dalam row pertama

--- app/src/main/java/com/example/tataletak/Tataletak1.kt
+++ app/src/main/java/com/example/tataletak/Tataletak1.kt
@@ -75,5 +75,10 @@ fun TataletakRowColumn(modifier: Modifier){
     Row(modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceEvenly) {
 //        kolom 1
+        Column() {
+            Text(text = "Komponen1Kolom1")
+            Text(text = "Komponen2Kolom1")
+            Text(text = "Komponen2Kolom1")
+        }
     }
 }
