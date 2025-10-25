fb9efdf0f600a84a7444e4ba665705a04c9d8c08
ZakyMalika <zakymalika0987@gmail.com>
2025-10-25 15:59:01 +0700

    membuat fun column + row

--- app/src/main/java/com/example/tataletak/Tataletak1.kt
+++ app/src/main/java/com/example/tataletak/Tataletak1.kt
@@ -49,3 +49,10 @@ fun TataletakBox(modifier: Modifier){
         Text(text = "Column 2")
     }
 }
+
+@Composable
+fun TataletakColumnRow(modifier: Modifier){
+    Column() {
+//        baris 1
+    }
+}
