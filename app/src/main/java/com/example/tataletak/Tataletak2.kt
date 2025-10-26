9673c3b4cb7891b67e98a52d47d29806f8dfec36
ZakyMalika <zakymalika0987@gmail.com>
2025-10-26 12:36:54 +0700

    melakukan perubahan warna circle 3

--- app/src/main/java/com/example/tataletak/Tataletak2.kt
+++ app/src/main/java/com/example/tataletak/Tataletak2.kt
@@ -122,7 +122,7 @@ fun Tambah(modifier: Modifier){
                     .height(height = 100.dp)
                     .width(width = 100.dp)
                     .clip(CircleShape)
-                    .background(color = Color.Cyan),
+                    .background(color = Color.Magenta),
                 contentAlignment = Alignment.Center
             )
 
